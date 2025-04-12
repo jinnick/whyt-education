559dd9841aa9ebbe6e236b39c9c995b34131687e
zyy <jy2913@columbia.edu>
2025-04-13 00:55:43 +0800

    更新配置和依赖

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/whyt-education/',
+    base: './',
     build: {
-        outDir: 'dist',
-        assetsDir: '.',
-        rollupOptions: {
-            output: {
-                assetFileNames: '[name][extname]',
-                chunkFileNames: 'assets/[name]-[hash].js',
-                entryFileNames: 'assets/[name]-[hash].js',
-            }
-        }
+        outDir: 'dist'
     }
 });
